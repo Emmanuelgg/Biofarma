673a795605f215f09b8921a204970ece3574df9d
Emmanuel Garcia <emmanuel.garcia.gtz@gmail.com>
2018-04-12 09:35:35 -0500

    plantilla general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,11 @@ import { NgModule, ApplicationRef } from '@angular/core';
 
 
 
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MapComponent } from './map/map.component';
-import { ContactComponent } from './contact/contact.component';
-
-
-import { AgmCoreModule } from '@agm/core';
-import { AppRoutingModule } from './/app-routing.module';
-import { SalesComponent } from './sales/sales.component';
-import { DateComponent } from './date/date.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
 
 // Import the Http Module and our Data Service
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
-
-import { AdminProductsComponent } from './admin-products/admin-products.component';
-import { FormProductsComponent } from './admin-products/form-products/form-products.component'
-import { ListProductsComponent } from './admin-products/list-products/list-products.component'
-
-import { AdminUsersComponent } from './admin-users/admin-users.component';
-import { FormUsersComponent } from './admin-users/form-users/form-users.component'
 
 // Import material.angular
 import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
@@ -39,25 +18,23 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 //import materil.angular animations
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 //Import dialog module
 import { DialogsModule } from './dialogs/dialogs.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { AppRoutingModule } from './/app-routing.module';
+
+//Import component
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    MapComponent,
-    ContactComponent,
-    SalesComponent,
-    DateComponent,
-    MenuComponent,
-    FooterComponent,
-    AdminProductsComponent,
-    FormProductsComponent,
-    ListProductsComponent,
-    AdminUsersComponent,
-    FormUsersComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,4 @@
-import { Component, OnInit, ViewChildren, QueryList} from '@angular/core';
-
-import {AppComponent} from "../app.component";
-
-import {Main} from "../main";
-
-import { DataService } from '../data.service';
-
-import { Title }     from '@angular/platform-browser';
-
-declare var jquery:any;
-declare var $ :any
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,46 +7,9 @@ declare var $ :any
 })
 export class DashboardComponent implements OnInit {
 
-  main: any = new Main;
-  items: Array<any>;
-  response: any;
-  @ViewChildren('carousel-item') carousel: QueryList<any>;
-
-  constructor(private _dataService: DataService, private titleService: Title) {
-    this._dataService.getTable('getAll','products', { _id: -1 }, 4)
-        .subscribe(res => {
-            this.response = res;
-            this.items = res.data;
-        });
-
-    this.titleService.setTitle( "Bio farma" );
-  }
+  constructor() { }
 
   ngOnInit() {
-
-  }
-
-  ngAfterViewInit() {
-    this.carousel.changes.subscribe(t => {
-      this.ngForRendred();
-    })
-  }
-
-  ngForRendred() {
-    this.initDashboard();
-  }
-
-  initDashboard(){
-    this.initCarousel();
-  }
-
-  initCarousel(){
-    $('.carousel.carousel-slider').carousel({fullWidth: true, duration: 400});
-    function autoplay() {
-        setTimeout(autoplay, 6500);
-        $('.carousel').carousel('next');
-    }
-    autoplay();
   }
 
 }
